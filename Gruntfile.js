6747fbf09c70a23548c77d75afde5d6b995fab6b
AlexCode <mail@alexcode.com>
2014-11-16 15:06:47 +0100

    Include versioning and minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,12 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    pkg: function() { return grunt.file.readJSON('package.json'); },
+    banner: '/*! <%= pkg().title || pkg().name %> - v<%= pkg().version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      '<%= pkg().homepage ? "* " + pkg().homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg().author.name %>;' +
+      ' Licensed <%= _.pluck(pkg().licenses, "type").join(", ") %> */\n',
     // Task configuration.
     clean: {
       src: ['dist']
@@ -20,8 +20,10 @@ module.exports = function(grunt) {
         banner: '<%= banner %>'
       },
       dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        files: {
+          'dist/IFrameOrchestrator.<%= pkg().version %>.min.js': ['src/IFrameOrchestrator.js'],
+          'dist/IFrameOrchestratorClient.<%= pkg().version %>.min.js': ['src/IFrameOrchestratorClient.js']
+        }
       },
     },
     jshint: {
@@ -31,17 +33,37 @@ module.exports = function(grunt) {
         },
         src: ['src/**/*.js']
       }
+    },
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin/master',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false
+      }
     }
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'clean', 'uglify']);
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('test', ['jshint', 'clean', 'uglify']);
+
+  grunt.registerTask('patch', ['jshint', 'bump-only:patch', 'clean', 'uglify', 'bump-commit']);
+  
   grunt.registerTask('travis', ['jshint']);
 
 };
